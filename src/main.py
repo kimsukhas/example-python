8414b09a01016d90f392e955adca74599081bc63
Koen Braham <kbraham@snt.utwente.nl>
2019-12-03 05:32:49 +0100

    Add main example

---
+++ src/main.py
@@ -0,0 +1,27 @@
+
+"""
+This example module shows various types of documentation available for use
+with pydoc.  To generate HTML documentation for this module issue the
+command:
+
+    pydoc -w foo
+
+"""
+
+class House:
+    """
+    House encapsulates a name and an age.
+    """
+    def __init__(self, name, age):
+        """
+        Construct a new House object.
+
+        :param name: The name of this house
+        :param age: The age of this house
+        :return: returns nothing
+        """
+        self.name = name
+        self.age = age
+
+if __name__ == '__main__':
+    h = House('Casa del koen', -1)
