190191baccacd6a1f66ec4767d135d727d19a807
Koen Braham <koen.braham@alten.nl>
2020-01-20 08:42:41 +0100

    Added test case and ci to repo

---
+++ src/house/test_house.py
@@ -0,0 +1,16 @@
+
+import unittest
+from house import House
+
+
+class HouseTestCase(unittest.TestCase):
+
+    def test_house_with_name_koen(self):
+        home = House("Koen", 10)
+
+        self.assertEqual(home.name.upper(), "KOEN")
+
+    def test_house_with_age_10(self):
+        home = House("Koen", 10)
+
+        self.assertEqual(home.age, 10)
